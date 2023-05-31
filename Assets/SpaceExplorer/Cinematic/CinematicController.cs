d678846e2d231ce6533f1fc4960c113d603ac532
Alexander Triguero <alex.triguero14@gmail.com>
2023-05-31 18:47:34 +0200

    Cinematic

---
+++ Assets/SpaceExplorer/Cinematic/CinematicController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CinematicController : MonoBehaviour
+{
+    [SerializeField] RotateAround rotate;
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
