a5d94ec282e74b1ba3b66c49715a6c0029ff9025
Alexander Triguero <alex.triguero14@gmail.com>
2023-06-14 10:35:11 +0200

    Small change to controller eventes and Update

--- Assets/SpaceExplorer/UI/MainMenu/Scripts/MenuControls.cs
+++ Assets/SpaceExplorer/UI/MainMenu/Scripts/MenuControls.cs
@@ -10,7 +10,10 @@ public class MenuControls : MonoBehaviour
     [SerializeField] float menuDeadZone = 0.5f;
     Button[] buttonList;
     int currentButtonIndex=0;
-    bool moved = false;
+    bool moveUp=false;
+    bool moveDown = false;
+
+
     // Start is called before the first frame update
     void Awake()
     {
@@ -22,28 +25,30 @@ public class MenuControls : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        moved = false;
+        if (moveDown)
+        {
+            currentButtonIndex = (currentButtonIndex + 1 < buttonList.Length) ? currentButtonIndex + 1 : 0;
+            buttonList[currentButtonIndex].Select();
+            moveDown = false;
+            moveUp = false;
+        }
+        else if (moveUp)
+        {
+            currentButtonIndex = (currentButtonIndex - 1 < 0) ? buttonList.Length - 1 : currentButtonIndex - 1;
+            buttonList[currentButtonIndex].Select();
+            moveUp = false;
+        }
     }
 
     public void NextButton(InputAction.CallbackContext context)
     {
-        float value = context.ReadValue<float>();
-        if (value >= menuDeadZone && !moved)
-        {
-            currentButtonIndex = (currentButtonIndex + 1 < buttonList.Length) ? currentButtonIndex + 1 : 0;
-            buttonList[currentButtonIndex].Select();
-            moved = true;
-        }
+        if (context.ReadValue<float>() >= menuDeadZone)
+            moveDown = true;    
     }
     public void PreviousButton(InputAction.CallbackContext context)
     {
-        float value = context.ReadValue<float>();
-        if (value >= menuDeadZone && !moved)
-        {
-            currentButtonIndex = (currentButtonIndex - 1 < 0) ? buttonList.Length - 1 : currentButtonIndex - 1;
-            buttonList[currentButtonIndex].Select();
-            moved = true;
-        }
+        if (context.ReadValue<float>() >= menuDeadZone)     
+            moveUp = true;
     }
     public void PressCurrentButton(InputAction.CallbackContext context)
     {
