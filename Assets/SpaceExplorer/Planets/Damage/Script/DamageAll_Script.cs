50c02c75cb4c82ba3e8e6e23a32e402ea688bbcf
Guillermo Yedra Doria <guillermoyedra@gmail.com>
2023-09-25 09:28:55 -0600

    actualizacion de daño

--- Assets/SpaceExplorer/Planets/Damage/Script/DamageAll_Script.cs
+++ Assets/SpaceExplorer/Planets/Damage/Script/DamageAll_Script.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DamageAll_Script : MonoBehaviour
 {
     [SerializeField] SphereCollider sphereCollider;
+    [SerializeField] float damage_ = 10000;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +18,16 @@ public class DamageAll_Script : MonoBehaviour
         Debug.Log(collision.gameObject);
         if (collision != null)
         {
-            Destroy(collision.gameObject);   
+            IDamageable damageable = collision.GetComponent<IDamageable>();
+            if (damageable != null)
+            {
+                damageable.NotifyHit(damage_);
+
+            }
+            else
+            {
+                Destroy(collision.gameObject);
+            }
         }
     }
 }
